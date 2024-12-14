bc34a3861c483e140275eee2be80fb7679f7f9fc
yonghun16 <yonghun16@naver.com>
2024-12-14 22:07:36 +0900

    your first server

---
+++ src/server.js
@@ -0,0 +1,9 @@
+import express from "express";
+
+const PORT = 4000
+const app = express();
+
+const handleListening = () => console.log(`Server listenting on port http://localhost:${PORT}`)
+
+app.listen(PORT, handleListening)
+
