b7ea58d06a804a624af0f53929f83761c031c134
yonghun16 <yonghun16@naver.com>
2024-12-22 00:12:52 +0900

    3.5 Middlewares part one

--- src/server.js
+++ src/server.js
@@ -4,15 +4,16 @@ const PORT = 4000
 
 const app = express();
 
+const gossipMiddleware = (req, res, next) => {
+  console.log("Middleware")
+  next()
+}
+
 const handleHome = (req, res) => {
   return res.send("I alive")
 }
 
-const handleLogin = (req, res) => {
-  return res.send("I login")
-}
-app.get("/", handleHome)
-app.get("/login", handleLogin)
+app.get("/", gossipMiddleware, handleHome)
 
 const handleListening = () => console.log(`Server listenting on port http://localhost:${PORT}`)
 
