7e68d52d2ec9f956dca21b75b4a8d68f99b2e868
yonghun16 <yonghun16@naver.com>
2024-12-21 19:39:33 +0900

    responses

--- src/server.js
+++ src/server.js
@@ -1,9 +1,21 @@
 import express from "express";
 
 const PORT = 4000
+
 const app = express();
+
+const handleHome = (req, res) => {
+  return res.send("I alive")
+}
+
+const handleLogin = (req, res) => {
+  return res.send("I login")
+}
+app.get("/", handleHome)
+app.get("/login", handleLogin)
 
 const handleListening = () => console.log(`Server listenting on port http://localhost:${PORT}`)
 
 app.listen(PORT, handleListening)
+
 
