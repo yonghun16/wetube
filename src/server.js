486215f866aa82352a7f02752ceecac977225a28
yonghun16 <yonghun16@naver.com>
2024-12-23 13:29:08 +0900

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,19 +1,21 @@
 import express from "express";
 
 const PORT = 4000
-
 const app = express();
 
-const gossipMiddleware = (req, res, next) => {
-  console.log("Middleware")
+
+const logger = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`)
   next()
 }
 
 const handleHome = (req, res) => {
-  return res.send("I alive")
+  return res.send("I love middlewares")
 }
 
-app.get("/", gossipMiddleware, handleHome)
+
+app.get("/", logger, handleHome)
+
 
 const handleListening = () => console.log(`Server listenting on port http://localhost:${PORT}`)
 
