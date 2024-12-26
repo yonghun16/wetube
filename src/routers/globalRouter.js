29e2a3c89a1c143e1da1475b4176a5ba11e3aff9
yonghun16 <yonghun16@naver.com>
2024-12-26 16:49:14 +0900

    4.3 Exports

--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -1,10 +1,11 @@
 import express from "express";
+import { join } from "../controllers/userController"
+import { trending } from "../controllers/videoController.js"
 
 const globalRouter = express.Router()
 
-const handleHome = (req, res) => res.send("Home")
-
-globalRouter.get("/", handleHome)
+globalRouter.get("/", trending)
+globalRouter.get("/join", join)
 
 export default globalRouter
 
