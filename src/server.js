84f18452d7fbe3153127c4d0772b05292ee51de1
yonghun16 <yonghun16@naver.com>
2024-12-26 12:43:14 +0900

    4.3 Exports

--- src/server.js
+++ src/server.js
@@ -1,29 +1,20 @@
-import express from "express";
-import morgan from "morgan";
+import express from "express"
+import morgan from "morgan"
+import globalRouter from "./routers/globalRouter"
+import userRouter from "./routers/userRouter"
+import videoRouter from "./routers/videoRouter"
+
 
 const PORT = 4000
 
-const app = express();
+const app = express()
 const logger = morgan("dev")
 app.use(logger)
 
-const globalRouter = express.Router()
-const handleHome = (req, res) => res.send("Home")
-globalRouter.get("/", handleHome)
-
-const userRouter = express.Router()
-const handleEditUser = (req, res) => res.send("Edit User")
-userRouter.get("/edit", handleEditUser)
-
-
-const videoRouter = express.Router()
-const handleWatchVideo = (req, res) => res.send("Watch video")
-videoRouter.get("/watch", handleWatchVideo)
 
 app.use("/", globalRouter)
 app.use("/videos", videoRouter)
 app.use("/users", userRouter)
-
 
 
 const handleListening = () => console.log(`Server listenting on port http://localhost:${PORT}`)
