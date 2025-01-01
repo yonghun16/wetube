194b164809ba50fc8b2187258100393d73b00f1a
yonghun16 <yonghun16@naver.com>
2025-01-01 21:52:38 +0900

    4.6 Planning routes

--- src/server.js
+++ src/server.js
@@ -4,7 +4,6 @@ import globalRouter from "./routers/globalRouter"
 import userRouter from "./routers/userRouter"
 import videoRouter from "./routers/videoRouter"
 
-
 const PORT = 4000
 
 const app = express()
