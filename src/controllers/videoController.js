bdc1d711f4cb467ac6c433ee56fe88ee01d51cf7
yonghun16 <yonghun16@naver.com>
2025-01-05 22:43:47 +0900

    5.8 Literation

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,11 +1,19 @@
-const fakeUser = {
-  username: "frontend16",
-  loggedIn: true
+export const trending = (req, res) => {
+  const videos = [
+    {
+      title: "hello",
+    },
+    {
+      title: "hello 2",
+    },
+    {
+      title: "hello 3",
+    },
+  ];
+  return res.render("home", { pageTitle: "home", videos });
 }
-
-export const trending = (req, res) => res.render("home", {pageTitle: "home", fakeUser })
-export const see = (req, res) => res.render("watch", {pageTitle: "Watch"})
-export const edit = (req, res) => res.render("edit", {pageTitle: "Edit"})
+export const see = (req, res) => res.render("watch", { pageTitle: "Watch" })
+export const edit = (req, res) => res.render("edit", { pageTitle: "Edit" })
 export const search = (req, res) => res.send("Search")
 export const upload = (req, res) => res.send("Upload")
 export const deleteVideo = (req, res) => res.send("Delete Video")
