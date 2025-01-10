29576be14d70af17d4a50773602f12b79554eb01
yonghun16 <yonghun16@naver.com>
2025-01-10 19:03:42 +0900

    edit video two

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -30,16 +30,19 @@ export const trending = (req, res) => res.render("home", { pageTitle: "home", vi
 export const watch = (req, res) => {
   const { id } = req.params;
   const video = videos[id - 1];
-  return res.render("watch", {pageTitle: `Watching ${video.title}`, video}); 
+  return res.render("watch", {pageTitle: `Watching: ${video.title}`, video}); 
 };
 
 export const getEdit = (req, res) => {
   const { id } = req.params;
   const video = videos[id - 1];
-  return res.render("edit", {pageTitle: `Editing ${video.title}`, video});
+  return res.render("edit", {pageTitle: `Editing: ${video.title}`, video});
 };
 
 export const postEdit = (req, res) => {
-  
+  const { id } = req.params;
+  const { title } = req.body;
+  videos[id - 1].title = title;
+  return res.redirect(`/videos/${id}`);  
 }
 
