1888b3d0e4fd42f30ada789438496ae295b36389
yonghun16 <yonghun16@naver.com>
2025-02-05 01:27:53 +0900

    Creating a Video part one

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,16 +1,13 @@
 import Video from "../models/Video";
 
-export const home = (req, res) => {
-  const fetchVideos = async () => {
-    try {
-      const videos = await Video.find({});
-      return res.render("home", { pageTitle: "Home", videos: videos });
-    } catch (error) {
-      console.error("errors:", error);
-    }
-  };
-  fetchVideos();
-}
+export const home = async (req, res) => {
+  try {
+    const videos = await Video.find({});
+    return res.render("home", { pageTitle: "Home", videos: videos });
+  } catch (error) {
+    console.error("errors:", error);
+  }
+};
 
 export const watch = (req, res) => {
   const { id } = req.params;
@@ -33,6 +30,17 @@ export const getUpload = (req, res) => {
 }
 
 export const postUpload = (req, res) => {
-  const { title } = req.body;
+  const { title, description, hashtags } = req.body;
+  const video = new Video({
+    title,
+    description,
+    createdAt: Date.now(),
+    hashtags: hashtags.split(",").map((word) => `#${word}`), 
+    meta: {
+      views: 0,
+      rating: 0
+    }
+  })
+  console.log(video);
   return res.redirect("/");
 }
