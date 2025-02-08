da8a397ef1b8c424ca993ef6a029b5ecf76d508e
yonghun16 <yonghun16@naver.com>
2025-02-08 17:19:37 +0900

    Delete video

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -3,6 +3,7 @@ import {
   watch,
   getUpload,
   getEdit,
+  deleteVideo,
   postEdit,
   postUpload,
 } from "../controllers/videoController";
@@ -11,6 +12,7 @@ const videoRouter = express.Router()
 
 videoRouter.get("/:id([0-9a-f]{24})", watch);
 videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit);
+videoRouter.route("/:id([0-9a-f]{24})/delete").get(deleteVideo);
 videoRouter.route("/upload").get(getUpload).post(postUpload);
 
 export default videoRouter;
