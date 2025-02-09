8b9ee9a3b72c39452a3a6a1788d88bf6a8aa6b5e
yonghun16 <yonghun16@naver.com>
2025-02-09 14:10:11 +0900

    create Acount

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,7 +1,26 @@
-export const join = (req, res) => res.send("Join")
-export const edit = (req, res) => res.send("Edit User")
-export const remove = (req, res) => res.send("Delete User")
-export const login = (req, res) => res.send("Login")
-export const logout = (req, res) => res.send("Logout")
-export const see = (req, res) => res.send("See User")
+import User from "../models/User";
+
+export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
+
+export const postJoin = async (req, res) => {
+  const { name, username, email, password, location } = req.body;
+  await User.create({
+    name,
+    username,
+    email,
+    password,
+    location
+  });
+  return res.redirect("/login");
+};
+
+export const edit = (req, res) => res.send("Edit User");
+
+export const remove = (req, res) => res.send("Delete User");
+
+export const login = (req, res) => res.send("Login");
+
+export const logout = (req, res) => res.send("Logout");
+
+export const see = (req, res) => res.send("See User");
 
