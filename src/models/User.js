d2d6a2838719d7ed206378ab9f4a844c191a762c
yonghun16 <yonghun16@naver.com>
2025-02-10 16:50:30 +0900

    hashing password

--- src/models/User.js
+++ src/models/User.js
@@ -1,5 +1,8 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
+
+// mongoose Schema 생성
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   username: { type: String, required: true, unique: true },
@@ -8,6 +11,16 @@ const userSchema = new mongoose.Schema({
   location: String,
 });
 
+// bcrypt를 사용하여 password 암호화
+  // Schema의  pre middleware 사용
+userSchema.pre('save', async function() {
+  console.log("Users password :", this.password);
+  this.password = await bcrypt.hash(this.password, 5);  
+  console.log("Hashed password :", this.password);
+})
+
+// mongoose Model 등록 (userSchema 사용)
 const User = mongoose.model("User", userSchema);
 
+// Model export
 export default User;
