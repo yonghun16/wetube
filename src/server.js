e0d7f48a0afdb269b2b9577d555451c1a7eb5c2f
yonghun16 <yonghun16@naver.com>
2025-03-01 00:54:05 +0900

    view

--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ import MongoStore from "connect-mongo"
 import rootRouter from "./routers/rootRouter"
 import userRouter from "./routers/userRouter"
 import videoRouter from "./routers/videoRouter"
+import apiRouter from "./routers/apiRouter"
 import { localsMiddleware } from "./middlewares"
 
 
@@ -33,5 +34,6 @@ app.use("/static", express.static("assets"))
 app.use("/", rootRouter)
 app.use("/videos", videoRouter)
 app.use("/users", userRouter)
+app.use("/api", apiRouter)
 
 export default app
