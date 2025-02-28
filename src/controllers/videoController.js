ed0d4276f89798e9311e5703f3df09843e491e31
yonghun16 <yonghun16@naver.com>
2025-02-28 18:55:47 +0900

    style

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -3,7 +3,9 @@ import User from "../models/User";
 
 export const home = async (req, res) => {
   try {
-    const videos = await Video.find({}).sort({ createdAt: "desc" });
+    const videos = await Video.find({})
+      .sort({ createdAt: "desc" })
+      .populate("owner");
     return res.render("home", { pageTitle: "Home", videos: videos });
   } catch (error) {
     console.error("errors:", error);
@@ -116,7 +118,7 @@ export const search = async (req, res) => {
       title: {
         $regex: new RegExp(keyword, "i"),
       },
-    });
+    }).populate("owner");
   }
   return res.render("search", { pageTitle: "Search", videos });
 }
